47a1ee4bef41e857b49b6d299402fc45ae99e2e5
Davi Batista <dsouza5@uolinc.com>
2024-09-10 12:08:53 -0300

    Pedindo as jogadores para escolherem seus pokemons

--- client_script.py
+++ client_script.py
@@ -1,24 +1,40 @@
 import socket
 import inquirer
 import typer
+from inquirer.themes import BlueComposure
 
 PORT = 4242
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
+
+def escolher_pokemons(cliente):
+    pokemons = [
+        inquirer.Checkbox("pokemons", message="Escolha seus pokémons (max 2)", choices=[
+            "Charmander", "Bulbasaur", "Squirtle", "Pikachu"
+        ], default=[])
+    ]
+
+    pokemons_escolhidos = inquirer.prompt(pokemons, theme=BlueComposure())
+
+    cliente.send(
+        f"pokemons_escolhidos|{pokemons_escolhidos['pokemons']}".encode("utf-8"))
+
+
 if __name__ == "__main__":
-  try:
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect(ADDR)
-  except Exception as e:
-    typer.echo(f"Erro ao conectar ao servidor: {e}")
-    exit()
-  
-  typer.echo("Bem vindo à batalha Pokémon!")
+    try:
+        cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cliente.connect(ADDR)
+    except Exception as e:
+        typer.echo(f"Erro ao conectar ao servidor: {e}")
+        exit()
 
-  nome = typer.prompt("Digite seu nome: ")
-  cliente.send(f"nome|{nome}".encode("utf-8"))
-  print("Conectado ao servidor!")
+    typer.echo("Bem vindo à batalha Pokémon!")
 
-  while True:
-    pass
+    nome = typer.prompt("Digite seu nome: ")
+    cliente.send(f"nome|{nome}".encode("utf-8"))
+    print("Conectado ao servidor!")
+
+    while True:
+        escolher_pokemons(cliente)
+        break
--- server_script.py
+++ server_script.py
@@ -10,11 +10,22 @@ servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind(ADDR)
 
 
+class Pokemon:
+    def __init__(self):
+        self.nome = None
+        self.tipo = None
+        self.ataques = [None]
+        self.vida = None
+        self.fraqueza = None
+        self.vantagem = None
+
+
 class Jogador:
     def __init__(self, socket):
         self.nome = None
         self.socket = socket
         self.socket_adversario = None
+        self.pokemons = None
 
     def set_nome(self, nome):
         self.nome = nome
@@ -26,7 +37,12 @@ def get_jogador_info(jogador):
     jogador.set_nome(nome)
     print(f"Jogador conectado: {jogador.nome}")
 
-    pass
+
+def define_pokemons(jogador):
+    pokemons_msg = jogador.socket.recv(1024).decode(FORMAT)
+    pokemons = pokemons_msg.split("|")[1]
+    print(f"Pokémons escolhidos por {jogador.nome}: {pokemons}")
+    jogador.pokemons = pokemons
 
 
 def main():
@@ -34,7 +50,7 @@ def main():
     print(f"Servidor rodando em {SERVER_IP}:{SERVER_PORT}")
     num_conexoes = 0
     while True:
-        socket_cliente = servidor.accept()
+        socket_cliente, endereco_cliente = servidor.accept()
         num_conexoes += 1
 
         if num_conexoes == 1:
@@ -42,11 +58,15 @@ def main():
             jogador1_thread = threading.Thread(target=get_jogador_info(
                 jogador1), args=(jogador1, socket_cliente))
             jogador1_thread.start()
+            define_pokemons(jogador1)
+
         else:
             jogador2 = Jogador(socket_cliente)
             jogador2_thread = threading.Thread(target=get_jogador_info(
                 jogador2), args=(jogador2, socket_cliente))
             jogador2_thread.start()
+
+            define_pokemons(jogador2)
 
 
 if __name__ == "__main__":
