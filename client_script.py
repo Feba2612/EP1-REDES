3664ce09f380f8b1473e9df5937b0be1e1c5b41c
Davi Batista <dsouza5@uolinc.com>
2024-09-10 11:23:53 -0300

    Criando a conexão de dois jogadores e pedindo nome

--- client_script.py
+++ client_script.py
@@ -1,13 +1,24 @@
 import socket
-import threading
+import inquirer
+import typer
 
 PORT = 4242
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+if __name__ == "__main__":
+  try:
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect(ADDR)
+  except Exception as e:
+    typer.echo(f"Erro ao conectar ao servidor: {e}")
+    exit()
+  
+  typer.echo("Bem vindo à batalha Pokémon!")
 
-msg = client.recv(1024).decode()
+  nome = typer.prompt("Digite seu nome: ")
+  cliente.send(f"nome|{nome}".encode("utf-8"))
+  print("Conectado ao servidor!")
 
-print(msg)
+  while True:
+    pass
--- server_script.py
+++ server_script.py
@@ -6,20 +6,49 @@ SERVER_PORT = 4242
 ADDR = (SERVER_IP, SERVER_PORT)
 FORMAT = "utf-8"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind(ADDR)
 
-def handle_client(connection, addr):
-    print(f"[NOVA CONEXÃO] {addr} conectado.")
 
-    connection.send(bytes("Bem-vindo ao servidor!", FORMAT))
-    connection.close()
+class Jogador:
+    def __init__(self, socket):
+        self.nome = None
+        self.socket = socket
+        self.socket_adversario = None
 
-def start():
-    server.listen()
+    def set_nome(self, nome):
+        self.nome = nome
+
+
+def get_jogador_info(jogador):
+    nome_msg = jogador.socket.recv(1024).decode(FORMAT)
+    nome = nome_msg.split("|")[1]
+    jogador.set_nome(nome)
+    print(f"Jogador conectado: {jogador.nome}")
+
+    pass
+
+
+def main():
+    servidor.listen(2)
+    print(f"Servidor rodando em {SERVER_IP}:{SERVER_PORT}")
+    num_conexoes = 0
     while True:
-        clientSocket, address = server.accept()
-        thread = threading.Thread(target=handle_client(clientSocket, address), args=(clientSocket, address))
-        thread.start()
+        socket_cliente = servidor.accept()
+        num_conexoes += 1
 
-start()
+        if num_conexoes == 1:
+            jogador1 = Jogador(socket_cliente)
+            jogador1_thread = threading.Thread(target=get_jogador_info(
+                jogador1), args=(jogador1, socket_cliente))
+            jogador1_thread.start()
+        else:
+            jogador2 = Jogador(socket_cliente)
+            jogador2_thread = threading.Thread(target=get_jogador_info(
+                jogador2), args=(jogador2, socket_cliente))
+            jogador2_thread.start()
+
+
+if __name__ == "__main__":
+    print("[INICIANDO] Servidor está iniciando...")
+    main()
